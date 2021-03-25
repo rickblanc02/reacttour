9cdb692beff37f1477879232d885a764a2b984a1
rickblanc02 <rickblanc02@gmail.com>
2021-03-25 10:41:52 -0400

    u

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-var path = require('path');
+/*var path = require('path');
 var express = require('express');
 
 var app = express();
@@ -8,4 +8,19 @@ app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
+});*/
+
+
+
+//Install express server
+const express = require('express');
+// Requirement of path Module in order to manage URLS
+const path = require('path');
+const app = express();
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/reacttour'));
+app.get('/*', function(req,res) {
+   res.sendFile(path.join(__dirname+'/dist/reacttour/index.html'));
 });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
