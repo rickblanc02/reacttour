d6c0e37b796e682eba159c51342e2cf4aa70b7c1
rickblanc02 <rickblanc02@gmail.com>
2021-03-25 12:09:02 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,8 @@ module.exports = {
         // global app config object
         config: JSON.stringify({
             apiUrl: 'http://localhost:4000',
-            //apiUrlTime: 'https://app-tours.herokuapp.com/api/v1/',
-            apiUrlTime: 'http://localhost:8080/api/v1/',            
+            apiUrlTime: 'https://app-tours.herokuapp.com/api/v1/',
+            //apiUrlTime: 'http://localhost:8080/api/v1/',            
             apiUrlTuten: 'https://dev.tuten.cl/TutenREST/rest/user/',
         })
     }
